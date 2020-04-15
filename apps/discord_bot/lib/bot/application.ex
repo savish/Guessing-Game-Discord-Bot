3684f92cdac888a93e69af3719b92ffa6c6ec513
Alan K <afksavish@gmail.com>
2020-04-15 18:09:10 +0200

    Initial discord bot code

--- apps/discord_bot/lib/bot/application.ex
+++ apps/discord_bot/lib/bot/application.ex
@@ -3,6 +3,7 @@ defmodule Bot.Application do
   use Application
   alias Alchemy.Client
   alias Guess.Server
+  alias Guess.Player
 
   defmodule Commands do
     use Alchemy.Cogs
@@ -16,6 +17,7 @@ defmodule Bot.Application do
       # Controls
 
       - !connect (?name) connect to the game server.
+      - !dc (?name) disconnect from the game server.
       ```
       """
 
@@ -27,12 +29,7 @@ defmodule Bot.Application do
     end
 
     Cogs.def connect() do
-      Cogs.say(
-        Cogs.member()
-        |> get_discord_name()
-        |> connnect_to_server()
-        |> response_fmt()
-      )
+      Cogs.say(Cogs.member() |> get_discord_name() |> connnect_to_server() |> response_fmt())
     end
 
     Cogs.def dc(player_name) do
@@ -43,11 +40,43 @@ defmodule Bot.Application do
       Cogs.say(Cogs.member() |> get_discord_name() |> disconnect_from_server() |> response_fmt())
     end
 
+    Cogs.def host(player_name) do
+      Cogs.say(player_name |> host_game() |> response_fmt())
+    end
+
+    Cogs.def host() do
+      Cogs.say(Cogs.member() |> get_discord_name() |> host_game() |> response_fmt())
+    end
+
+    Cogs.def join(player_name, host_name) do
+      Cogs.say(player_name |> join_game(host_name) |> response_fmt())
+    end
+
+    Cogs.def join(host_name) do
+      Cogs.say(Cogs.member() |> get_discord_name() |> join_game(host_name) |> response_fmt())
+    end
+
+    Cogs.def start(player_name) do
+      Cogs.say(player_name |> start_game() |> response_fmt())
+    end
+
+    Cogs.def start() do
+      Cogs.say(Cogs.member() |> get_discord_name() |> start_game() |> response_fmt())
+    end
+
+    Cogs.def play(player_name, guess) do
+      Cogs.say(player_name |> play_game(guess) |> response_fmt())
+    end
+
+    Cogs.def play(guess) do
+      Cogs.say(Cogs.member() |> get_discord_name() |> play_game(guess) |> response_fmt())
+    end
+
     Cogs.def players() do
-      {:ok, resp_arr} = Server.list_players()
+      players = Server.list_players()
 
       response =
-        resp_arr
+        players
         |> Enum.with_index(1)
         |> Enum.map(fn {index, player} -> "#{player}. #{index}" end)
         |> Enum.join("\n")
@@ -56,25 +85,13 @@ defmodule Bot.Application do
     end
 
     Cogs.def games() do
-      {:ok, resp_arr} = Server.list_games()
+      games = Server.list_games()
 
       response =
-        resp_arr
+        games
         |> Enum.with_index(1)
         |> Enum.map(fn {index, game} -> "#{game}. #{index}" end)
         |> Enum.join("\n")
-
-      Cogs.say(response)
-    end
-
-    Cogs.def host() do
-      {:ok, %Alchemy.Guild.GuildMember{user: %Alchemy.User{username: username}}} = Cogs.member()
-
-      response =
-        case Server.host_game(username) do
-          {:error, _} -> "Unable to host game"
-          _ -> "Game hosted"
-        end
 
       Cogs.say(response)
     end
@@ -95,7 +112,7 @@ defmodule Bot.Application do
 
     @spec connnect_to_server(String.t()) :: String.t()
     defp connnect_to_server(player_name) do
-      case Server.join(player_name) do
+      case Server.connect(player_name) do
         {:error, :player_name_taken} ->
           "Player name taken! Please choose another using '!connect <<name>>'"
 
@@ -109,12 +126,61 @@ defmodule Bot.Application do
 
     @spec disconnect_from_server(String.t()) :: String.t()
     defp disconnect_from_server(player_name) do
-      case Server.leave(player_name) do
+      case Server.dc(player_name) do
         {:error, :player_doesnt_exist} ->
           "#{player_name} is not connected to this server"
 
         :ok ->
           "#{player_name} has left the server."
+      end
+    end
+
+    @spec host_game(String.t()) :: String.t()
+    defp host_game(player_name) do
+      case Server.host(player_name) do
+        {:ok, summary} -> summary
+        {:error, :game_in_progress} -> "This game is already in progress"
+        {:error, other} -> other
+      end
+    end
+
+    @spec join_game(String.t(), String.t()) :: String.t()
+    defp join_game(player_name, host_name) do
+      case Server.join(player_name, host_name) do
+        :ok -> "Joined #{host_name}'s game"
+        _ -> "Unable to join #{host_name}'s game"
+      end
+    end
+
+    @spec start_game(String.t()) :: String.t()
+    defp start_game(player_name) do
+      case Player.start_game(player_name) do
+        {:ok, {:round, round, :turn, turn}} ->
+          "Game started!\n==\nRound #{round}, #{turn}'s turn\nEnter your guess: "
+
+        {:error, :player_isnt_host} ->
+          "Only the host can start a game"
+
+        {:error} ->
+          "Unable to start the game"
+      end
+    end
+
+    defp play_game(player_name, guess) do
+      case Player.play(player_name, String.to_integer(guess)) do
+        {:end_turn, {:round, round, :turn, turn}} ->
+          "Round #{round}, #{turn}'s turn\nEnter your guess: "
+
+        {:end_round, {{:round, round, :turn, turn}, points}} ->
+          point_string = Enum.reduce(points, "", fn {k, v}, acc -> "#{acc}#{k} => #{v} " end)
+
+          "Round finished.\nCurrent points: #{point_string}\n\nNew round\n==\nRound #{round}, #{
+            turn
+          }'s turn\nEnter your guess: "
+
+        {:end_round, {:ended, points}} ->
+          point_string = Enum.reduce(points, "", fn {k, v}, acc -> "#{acc}#{k} => #{v} " end)
+          "Game over!!\nFinal score: #{point_string}"
       end
     end
   end
